55b3d7eb0348f2d35fb9ad9aca8ee87a815861be
Beketovian <andrewbeketov03@gmail.com>
2023-04-10 23:21:07 -0500

    navbar fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,22 +11,20 @@ import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const NavButton = ({title, customFunc, icon, color, dotColor }) => (
-  <TooltipComponent content={title}
-  position='BottomCenter'>
+  <TooltipComponent content={title} position='BottomCenter'>
     <button type='button' onClick={customFunc}
-    style={{ color}}
+    style={{ color }}
     className='relative text-xl rounded-full p-3 hover:bg-light-gray'
     >
       <span style={{ background: dotColor }}
       className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'
-      >
+      />
         {icon}
-      </span>
       
 
     </button>
   </TooltipComponent>
-)
+);
 const Navbar = () => {
   const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } = useStateContext();
 
